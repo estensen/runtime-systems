857fad0429884352be140914a0035617717cc9d7
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-04-28 11:52:53 -0700

    Get filenames of report files for CPU

--- backend/app.go
+++ backend/app.go
@@ -11,6 +11,35 @@ import (
 
 func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	payload := map[string]string{"apiType": "This is a RESTful API"}
+	respondWithJSON(w, http.StatusOK, payload)
+}
+
+func getCPU(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	dirname := "reports"
+
+	f, err := os.Open(dirname)
+	if err != nil {
+		respondWithJSON(w, http.StatusBadRequest, "Could not open report files")
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		respondWithJSON(w, http.StatusBadRequest, "Could not read report files")
+	}
+
+	var filenames []string
+
+	for _, file := range files {
+		filenames = append(filenames, file.Name())
+	}
+
+	response := map[string][]string{"filenames": filenames}
+
+	respondWithJSON(w, http.StatusOK, response)
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, err := json.Marshal(payload)
 	if err != nil {
 		panic(err)
@@ -19,7 +48,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	enableCors(&w)
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(code)
 	w.Write(response)
 }
 
@@ -30,6 +59,7 @@ func enableCors(w *http.ResponseWriter) {
 func main() {
 	router := httprouter.New()
 	router.GET("/", indexHandler)
+	router.GET("/cpu", getCPU)
 
 	env := os.Getenv("APP_ENV")
 	if env == "production" {
