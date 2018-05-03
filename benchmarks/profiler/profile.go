2fd083dcfd3b1b69c8ccf939902adae995c745b1
mariannemagnussen <marianne_magnussen@icloud.com>
2018-05-03 10:20:09 -0700

    move fibonacci to own package and directory

--- benchmarks/profiler/profile.go
+++ benchmarks/profiler/profile.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"fmt"
 	_ "net/http/pprof"
 
+	"github.com/estensen/runtime-systems/benchmarks/fibonacci"
 	"github.com/pkg/profile"
 )
-
-func fibonacci(n int) {
-	a := 0
-	b := 1
-	for i := 0; i < n; i++ {
-		a, b = b, a+b
-
-		fmt.Println(b)
-	}
-}
 
 func main() {
 	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 
-	fibonacci(1000000)
+	fibonacci.Fibonacci(1000000)
 }
