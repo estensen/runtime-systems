f546a3779506c4a92a4759be79c2f2f589cf5230
mariannemagnussen <marianne_magnussen@icloud.com>
2018-05-03 17:01:01 -0700

    remove hello call

--- benchmarks/main.go
+++ benchmarks/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"github.com/estensen/runtime-systems/benchmarks/hello"
-
 	"github.com/estensen/runtime-systems/benchmarks/profiler"
 )
 
 func main() {
 	profiler.Profiler()
-	hello.Hello()
 }
