5bbb0dc6b52bfc49b1dfc9cc9c928190b3bdf683
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-05-15 15:07:14 -0700

    Measure CPU load

--- backend/benchmarks/profiler/profile.go
+++ backend/benchmarks/profiler/profile.go
@@ -2,9 +2,12 @@ package profiler
 
 import (
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/estensen/runtime-systems/backend/benchmarks/programs/sort"
 	"github.com/pkg/profile"
+	"github.com/shirou/gopsutil/cpu"
 )
 
 //Profiler running from main
@@ -15,6 +18,7 @@ func Profiler(packageName string) {
 func runPackage(packageName string) {
 	//checkPackage
 	//checkPackageTest
+	go CPUPercent()
 	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 	switch packageName {
 	case "sort":
@@ -22,4 +26,24 @@ func runPackage(packageName string) {
 	default:
 		fmt.Println("Package not found")
 	}
+}
+
+func CPUPercent() {
+	filename := "cpupercent.csv"
+
+	c, _ := cpu.Percent(500*time.Millisecond, false)
+
+	t := time.Now()
+	now := t.Format("2006-01-02 15:04:05")
+
+	// Write file as <timestamp>,<size>,<board>
+	line := []string{now, c} // Convert c from float64 to string
+
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
+	if err != nil {
+		panic("could not open csv file")
+	}
+
+	defer file.Close()
+
 }
