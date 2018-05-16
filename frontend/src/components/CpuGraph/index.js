03a6718c9d0cf6ed7c955d67cace4aa3f51db45e
mariannemagnussen <marianne_magnussen@icloud.com>
2018-05-16 15:29:54 -0700

    temp graph

--- frontend/src/components/CpuGraph/index.js
+++ frontend/src/components/CpuGraph/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+const API = 'http://localhost:8080/cpu/live/sort'
+
 const exampleData = {
     labels: ['8:05', '8:10', '8:15', '8:20'],
     datasets: [
@@ -16,18 +18,50 @@ const exampleData = {
 
 var LineChart = require("react-chartjs").Line;
 
-//const API = 'http://localhost:8080/cpu/livedata/'
-
 class CpuGraph extends Component {
+
   constructor(props) {
     super(props)
+
+    this.state = {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          fillColor: "rgba(220,220,220,0)",
+          strokeColor: 'rgba(50,80,220,1)',
+          pointColor: 'rgba(50,80,220,1)',
+          pointHighlightFill: '#fff',
+        }
+      ],
+      jsondata: [],
+    }
+  }
+
+  componentDidMount() {    
+    
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          response.json()
+          .then(jsondata => this.setState({ jsondata: [...jsondata]}))
+        }
+      })
+  }
+
+  updateGraphData(label, cpuPercent) {
+    this.state.labels.push(label)
+    this.state.datasets[0].data.push(cpuPercent)
+    console.log(label, cpuPercent)
   }
 
   render() {
+    const { jsondata } = this.state
 
     return (
       <div>
-        <LineChart data={exampleData} width="600" height="250"/>
+        {jsondata}
+        <LineChart data={this.state} width="600" height="250"/>
       </div>
     )
   }
