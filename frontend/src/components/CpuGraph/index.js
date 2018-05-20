15ee7645ad2bb154d8f43b69e21658c8f27eab2c
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-05-20 15:42:11 -0700

    Prepare to read graph data from lists

--- frontend/src/components/CpuGraph/index.js
+++ frontend/src/components/CpuGraph/index.js
@@ -3,70 +3,46 @@ import React, { Component } from 'react'
 var LineChart = require("react-chartjs").Line
 const API = 'http://localhost:8080/cpu/live/sort'
 
-const chart1 = {
-  labels: ['Team1', 'Team2', 'Team3', 'Team4', 'Team5'],
+const chart = {
+  labels: ['8:05', '8:10', '8:15', '8:20', '8:25'],
   datasets: [{
-    label: 'Team points',
-    data: [503, 385, 270, 133, 65],
-    backgroundColor: [
-      '#4DB6AC',
-      '#E57373',
-      '#7986CB',
-      '#F06292',
-      '#E0E0E0'
-    ]
-  }]
-}
-
-const chart2 = {
-  labels: ['Team1', 'Team2', 'Team3', 'Team4', 'Team5'],
-  datasets: [{
-    label: 'Team points 2',
+    label: 'CPU Utilization',
     data: [303, 185, 470, 313, 65],
-    backgroundColor: [
-      '#4DB6AC',
-      '#E57373',
-      '#7986CB',
-      '#F06292',
-      '#E0E0E0'
-    ]
+    fillColor: "rgba(220,220,220,0)",
+    strokeColor: 'rgba(50,80,220,1)',
+    pointColor: 'rgba(50,80,220,1)',
+    pointHighlightFill: '#fff',
   }]
 }
 
-const Button = props => (
-  <button id="update-chart" onClick={props.handleOnClick}>Update</button>
-)
 
 class CpuGraph extends Component {
   constructor(props) {
     super(props)
 
-    this.handleUpdate = this.handleUpdate.bind(this)
-    this.update = false
-
     this.state = {
-      chartData: chart1
+      chartData: chart
     }
 
     console.log(this.state.chartData)
     console.log(this.updated)
   }
 
-  handleUpdate() {
-    const chartData = this.updated ? chart1 : chart2
-    this.setState({chartData}, () => {
-      this.updated = this.updated ? false : true
-      console.log(this.state.chartData)
-    })
+  updateState(newData) {
+    this.setState(prevState => ({
+      ...prevState,
+      labels: ['8:05', '8:10', '8:15', '8:20', '8:25'],
+      datasets: {
+        ...prevState.datasets,
+        data: [303, 185, 470, 313, 65]
+      }
+    }))
   }
 
   render() {
-    // const { jsondata } = this.state
-
     return (
       <div>
         <LineChart data={this.state.chartData} width="600" height="250"/>
-        <Button handleOnClick={this.handleUpdate} />
       </div>
     )
   }
