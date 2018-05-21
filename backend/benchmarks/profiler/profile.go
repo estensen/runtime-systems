c90da96ed1bebd45cd306e1e002d34c4ffe4c876
mariannemagnussen <marianne_magnussen@icloud.com>
2018-05-21 14:11:47 -0700

    remove commented code

--- backend/benchmarks/profiler/profile.go
+++ backend/benchmarks/profiler/profile.go
@@ -39,28 +39,4 @@ func CPUPercent() []string {
 	now := t.Format("15:04:05")
 	line := []string{now, strconv.FormatFloat(c[0], 'f', 2, 64)} // Convert c from float64 to string
 	return line
-
-	/*
-		filename := "cpupercent.csv"
-
-		c, _ := cpu.Percent(10*time.Millisecond, false)
-		t := time.Now()
-		now := t.Format("2006-01-02 15:04:05")
-
-		// Write file as <timestamp>,<size>,<board>
-		line := []string{now, strconv.FormatFloat(c[0], 'f', 2, 64)} // Convert c from float64 to string
-
-		file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
-		if err != nil {
-			panic("could not open csv file")
-		}
-
-		defer file.Close()
-
-		json, err = json.Marshal(line)
-		if err != nil {
-			panic(err)
-		}
-
-		file.Write(json)*/
 }
