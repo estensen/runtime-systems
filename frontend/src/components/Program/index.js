f1247b901e3dd86c2bc2ad1ba59b145886aa7446
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-05-23 14:33:05 -0700

    Add paper component

--- frontend/src/components/Program/index.js
+++ frontend/src/components/Program/index.js
@@ -1,31 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
-class CpuProgram extends Component {
-  render() {
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    width: 400,
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+  }),
+})
 
-    const { match: { params: { programName } } } = this.props
+function CpuProgram(props) {
+  const { match: { params: { programName } } } = props
+  const { classes } = props
 
-    return (
+  return (
     <div>
-      <h1>CPU</h1>
-      <h2>{programName}</h2>
-      <div>
-          <ul>
-            <li key="Diagram">
-              <Link to={`${programName}/diagram`}>Diagram</Link>
-            </li>
-            <li key="Graph">
-              <Link to={`${programName}/graph`}>Graph</Link>
-            </li>
-            <li key="Report">
-              <Link to={`${programName}/report`}>Report</Link>
-            </li>
-          </ul>
-        </div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography variant="headline" component="h3">
+          <h1>CPU</h1>
+          <h2>{programName}</h2>
+          <div>
+              <ul>
+                <li key="Diagram">
+                  <Link to={`${programName}/diagram`}>Diagram</Link>
+                </li>
+                <li key="Graph">
+                  <Link to={`${programName}/graph`}>Graph</Link>
+                </li>
+                <li key="Report">
+                  <Link to={`${programName}/report`}>Report</Link>
+                </li>
+              </ul>
+          </div>
+        </Typography>
+      </Paper>
     </div>
-    )
-  }
+  )
 }
 
-export default CpuProgram
+CpuProgram.PropTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(CpuProgram)
