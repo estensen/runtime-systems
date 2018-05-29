753d395b42772ad6a3a27e9d065200fb80c54c3c
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-05-29 13:32:15 -0700

    Add paper component to home

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 const API = 'http://localhost:8080'
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    width: 400,
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+  }),
+})
 
 class Home extends Component {
   constructor(props) {
@@ -18,15 +31,25 @@ class Home extends Component {
   }
 
   render() {
+    const { classes } = this.props
     const { apiType } = this.state
 
     return (
       <div>
-        <h1>Home</h1>
-        {apiType}
+        <Paper className={classes.root} elevation={4}>
+          <Typography variant="headline" component="h3">
+            <h1>Home</h1>
+            {apiType}
+            Add instructions on how to use the app here
+          </Typography>
+        </Paper>
       </div>
     )
   }
 }
 
-export default Home
+Home.PropTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Home)
